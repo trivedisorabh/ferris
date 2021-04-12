583b4853a9d5b99840f122b110db479c3f66ae5d
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2021-04-12 15:00:41 +0200

    Added Percy and Chromatic (#2)
    * Added percy
    
    * percy updates
    
    * updated the text color
    
    * updated the text color
    
    * updated the text color
    
    * added chromatic

--- src/hello-world/hello-world.ts
+++ src/hello-world/hello-world.ts
@@ -5,7 +5,7 @@ export class HelloWorld extends LitElement {
     :host {
       display: block;
       padding: 25px;
-      color: var(--hello-world-text-color, #000);
+      color: var(--hello-world-text-color, #00a);
     }
   `;
 
