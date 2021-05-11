dfed5b40c106c47c25bb42bea75fb97f0fafddde
stt-tomahl <80753901+stt-tomahl@users.noreply.github.com>
2021-05-11 11:27:39 +0200

    Style Dictonary: Export ts-files instead of js (#52)

---
+++ src/tokens/all.ts
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 11 May 2021 07:31:50 GMT
+ */
+
+export const UiElementsPrimary200Selected = '#E7EEFA'; // Used in table rows, list etc
+export const UiElementsPrimary300Hover = '#CFDEF7'; // Used for hover state
+export const ColorBrandPrimary = '#4f98a4';
+export const ColorCore600 = '#cccccc';
+export const ColorCoreGreen = '#4f98a4';
+export const SizeFontLarge = '24rem';
+export const SizeFontMedium = '16rem';
+export const SizeFontSmall = '12rem';
---
+++ src/tokens/color.ts
@@ -0,0 +1,8 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 11 May 2021 07:31:50 GMT
+ */
+
+export const ColorBrandPrimary = '#4f98a4';
+export const ColorCore600 = '#cccccc';
+export const ColorCoreGreen = '#4f98a4';
