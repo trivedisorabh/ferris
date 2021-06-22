1597035a6079fca02ae58c84a3fe262cc5442e36
stt-tomahl <tommy.ahlback@sigmatechnology.se>
2021-06-22 16:42:52 +0200

    Feature(Icon): Added initial file

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,48 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import React, { ForwardedRef, forwardRef, HTMLAttributes, ReactChild } from 'react';
+import Colors from '~tokens/colors/Colors';
+import Sizes from '~tokens/sizes/Sizes';
+
+/**
+ * @category Props
+ */
+export interface IconProps extends Omit<HTMLAttributes<HTMLSpanElement>, 'children'> {
+	icon: ReactChild;
+	color?: Colors;
+	size?: Sizes.size_1 | Sizes.size_1_25;
+}
+
+/**
+ * @category Template
+ */
+const Icon = forwardRef(
+	(
+		{ color, icon, size = Sizes.size_1, ...rest }: IconProps,
+		ref: ForwardedRef<HTMLSpanElement>
+	) => (
+		<StyledIcon {...rest} aria-hidden color={color} data-tpl="Icon" ref={ref} size={size}>
+			{icon}
+		</StyledIcon>
+	)
+);
+
+Icon.displayName = 'Icon';
+export default Icon;
+
+/**
+ * @category Styles
+ */
+type StyledIconProps = Omit<IconProps, 'icon'>;
+
+const StyledIcon = styled.span(
+	({ color, size }: StyledIconProps) => css`
+		align-items: center;
+		color: ${color};
+		display: inline-flex;
+		height: ${size};
+		justify-content: center;
+		user-select: none;
+		width: ${size};
+	`
+);
