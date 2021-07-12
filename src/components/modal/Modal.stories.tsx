d9c9f0abb3a04231a39e5de9fb137031edf89cf8
Tobias Ljungström <tobias.ljungstrom@gmail.com>
2021-07-12 15:39:33 +0200

    Feature(Modal): Add header and close button
    Conditionally render header and button.
    Conditionally style header.
    Separate onClose callback from button visibility.
    Remove stylelint rule that interfers with variables.

--- src/components/modal/Modal.stories.tsx
+++ src/components/modal/Modal.stories.tsx
@@ -2,20 +2,48 @@ import React, { useState } from 'react';
 import { Story } from '~common/interfaces';
 import Button from '~components/button/Button';
 import Modal, { ModalProps } from '~components/modal/Modal';
+import Row from '~components/row/Row';
+import Spacer from '~components/spacer/Spacer';
+import Spacings from '~tokens/spacings/Spacings';
 
 export default {
 	title: 'Components/Modal',
 	component: Modal,
 };
 
-const Template: Story<ModalProps> = ({ open }: ModalProps) => {
+const Template: Story<ModalProps> = ({ open, children = 'Content', onClose }: ModalProps) => {
 	const [isOpen, setIsOpen] = useState(open);
 
 	return (
 		<>
 			<Button onClick={() => setIsOpen(true)}>Open Modal</Button>
 			<Modal open={isOpen} onClose={() => setIsOpen(false)}>
-				This is teh content of the modal
+				{children}
+			</Modal>
+		</>
+	);
+};
+
+const TemplateCustomContent: Story<ModalProps> = ({ showCloseButton, headerText }: ModalProps) => {
+	const [isOpen, setIsOpen] = useState(true);
+
+	return (
+		<>
+			<Button onClick={() => setIsOpen(true)}>Open Modal</Button>
+			<Modal
+				open={isOpen}
+				showCloseButton={showCloseButton}
+				headerText={headerText}
+				onClose={() => {
+					setIsOpen(false);
+				}}
+			>
+				<p>Here is a a question for you: Do you want to close this modal?</p>
+				<Row alignX="end">
+					<Button onClick={() => setIsOpen(false)}>Yes</Button>
+					<Spacer spacing={Spacings.sm} />
+					<Button onClick={() => setIsOpen(false)}>No, but actually yes</Button>
+				</Row>
 			</Modal>
 		</>
 	);
@@ -23,6 +51,12 @@ const Template: Story<ModalProps> = ({ open }: ModalProps) => {
 
 export const Open = Template.bind({});
 Open.args = { open: true };
+
+export const CustomContent = TemplateCustomContent.bind({});
+CustomContent.args = { showCloseButton: false };
+
+export const CustomContentAndHeader = TemplateCustomContent.bind({});
+CustomContentAndHeader.args = { showCloseButton: true, headerText: 'Listen!' };
 
 export const Closed = Template.bind({});
 Closed.args = {};
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,15 +1,21 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import React, { HTMLAttributes, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import Heading from '~components/heading/Heading';
+import Icon from '~components/icon/Icon';
 import Colors from '~tokens/colors/Colors';
+import Icons from '~tokens/icons/Icons';
 import Spacings from '~tokens/spacings/Spacings';
 
 export interface ModalProps extends HTMLAttributes<HTMLDivElement> {
 	open: boolean;
 	onClose?: () => void;
+	showCloseButton?: boolean;
+	headerText?: string;
 }
 
-const Modal = ({ open, onClose, children }: ModalProps) => {
+const Modal = ({ open, onClose, showCloseButton, headerText, children }: ModalProps) => {
 	if (!open) return null;
 
 	const bodyElement = document.querySelector('body');
@@ -29,7 +35,7 @@ const Modal = ({ open, onClose, children }: ModalProps) => {
 		}
 	}
 
-	useEffect(function closeOnEscape() {
+	useEffect(function keyboardListener() {
 		document.addEventListener('keyup', handleKeyUp);
 		return () => {
 			document.removeEventListener('keyup', handleKeyUp);
@@ -37,12 +43,23 @@ const Modal = ({ open, onClose, children }: ModalProps) => {
 	}, []);
 
 	return ReactDOM.createPortal(
-		<ModalBackground>
+		<>
+			<ModalBackground></ModalBackground>
+
 			<ModalWindow>
-				{onClose && <button onClick={closeModal}>close</button>}
-				{children}
+				{(headerText || showCloseButton) && (
+					<ModalHeaderRow headerText={headerText}>
+						{headerText && <Heading as="h1">{headerText}</Heading>}
+						{showCloseButton && (
+							<ModalCloseButton onClick={closeModal}>
+								<Icon icon={Icons.Close} />
+							</ModalCloseButton>
+						)}
+					</ModalHeaderRow>
+				)}
+				<ModalContentWrapper>{children}</ModalContentWrapper>
 			</ModalWindow>
-		</ModalBackground>,
+		</>,
 		bodyElement
 	);
 };
@@ -59,10 +76,39 @@ const ModalBackground = styled.div`
 	top: 0;
 `;
 
+/*  Absolute position centering is used to center the window without having
+		access to modify its parent. */
 const ModalWindow = styled.div`
 	background-color: ${Colors.white};
+	left: 50%;
+	max-height: 50vh;
 	min-height: 200px;
 	min-width: 400px;
+	overflow-y: auto;
+	position: absolute;
+	top: 50%;
+	transform: translate(-50%, -50%);
+`;
+
+type ModalHeaderRowProps = Pick<ModalProps, 'headerText'>;
+
+const ModalHeaderRow = styled.div(
+	({ headerText }: ModalHeaderRowProps) => css`
+		background-color: ${headerText ? Colors.brandBase : 'transparent'};
+		color: ${headerText ? Colors.white : Colors.blackBrand};
+		display: flex;
+		justify-content: ${headerText ? 'space-between' : 'end'};
+		padding: ${Spacings.lg};
+	`
+);
+
+const ModalCloseButton = styled.button`
+	background: none;
+	border: none;
+	padding: 0;
+`;
+
+const ModalContentWrapper = styled.div`
 	padding: ${Spacings.lg};
 `;
 
