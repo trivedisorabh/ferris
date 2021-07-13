4ce81ec11281a71e04f16e075c19976d790f365f
Tobias Ljungström <tobias.ljungstrom@gmail.com>
2021-07-13 14:05:39 +0200

    Feature(Modal): Separate heading tag and styling
    This was necessary short term for modal styling, long term
    for healthy component use.

--- src/atoms/heading/Heading.tsx
+++ src/atoms/heading/Heading.tsx
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import React, { ForwardedRef, forwardRef, HTMLAttributes, ReactNode } from 'react';
+import FontSizes from '~tokens/font-sizes/FontSizes';
 import FontWeights from '~tokens/font-weights/FontWeights';
 import Headings from '~tokens/headings/Headings';
 import LineHeights from '~tokens/line-heights/LineHeights';
@@ -10,17 +11,26 @@ import LineHeights from '~tokens/line-heights/LineHeights';
  */
 export type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4';
 
+export type HeadingStyle = {
+	fontSize?: FontSizes;
+	fontWeight?: FontWeights;
+};
+
 export interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
 	as: HeadingLevel;
 	children: ReactNode;
+	headingStyle?: HeadingStyle;
 }
 
 /**
  * @category Template
  */
 const Heading = forwardRef(
-	({ as, children, ...rest }: HeadingProps, ref: ForwardedRef<HTMLHeadingElement>) => (
-		<StyledHeading {...rest} as={as} ref={ref}>
+	(
+		{ as, children, headingStyle, ...rest }: HeadingProps,
+		ref: ForwardedRef<HTMLHeadingElement>
+	) => (
+		<StyledHeading {...rest} as={as} ref={ref} headingStyle={headingStyle}>
 			{children}
 		</StyledHeading>
 	)
@@ -32,10 +42,10 @@ export default Heading;
 /**
  * @category Styles
  */
-type StyledHeadingProps = Pick<HeadingProps, 'as'>;
+type StyledHeadingProps = Pick<HeadingProps, 'as' | 'headingStyle'>;
 
 const StyledHeading = styled.h1(
-	({ as }: StyledHeadingProps) => css`
+	({ as, headingStyle }: StyledHeadingProps) => css`
 		line-height: ${LineHeights.compact};
 
 		${as === 'h1' &&
@@ -44,24 +54,32 @@ const StyledHeading = styled.h1(
 			font-weight: ${Headings.h1Weight};
 		`}
 
-		${(as === 'h2' || as === 'h3' || as === 'h4') &&
-		css`
-			font-weight: ${FontWeights.bold};
-		`}
-
 		${as === 'h2' &&
 		css`
 			font-size: ${Headings.h2Size};
+			font-weight: ${Headings.h2Weight};
 		`}
 
 		${as === 'h3' &&
 		css`
 			font-size: ${Headings.h3Size};
+			font-weight: ${Headings.h3Weight};
 		`}
 
 		${as === 'h4' &&
 		css`
 			font-size: ${Headings.h4Size};
+			font-weight: ${Headings.h4Weight};
+		`}
+
+		${headingStyle?.fontSize &&
+		css`
+			font-size: ${headingStyle.fontSize};
+		`}
+
+		${headingStyle?.fontWeight &&
+		css`
+			font-weight: ${headingStyle.fontWeight};
 		`}
 	`
 );
