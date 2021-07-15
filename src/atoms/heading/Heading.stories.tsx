cba798b5b861ab4ae00ac87346c2b02c35106214
Tobias Ljungström <tobias.ljungstrom@gmail.com>
2021-07-15 17:33:37 +0200

    Feature(Heading): Revert enum, spruce up tests

--- src/atoms/heading/Heading.stories.tsx
+++ src/atoms/heading/Heading.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Heading, { HeadingLevel, HeadingProps } from '~atoms/heading/Heading';
+import Heading, { HeadingProps } from '~atoms/heading/Heading';
 import { Story } from '~common/interfaces';
 
 export default {
@@ -15,40 +15,40 @@ const Template: Story<HeadingProps> = ({ as, children, styledAs, ...rest }: Head
 
 export const DefaultH1 = Template.bind({});
 DefaultH1.args = {
-	as: HeadingLevel.h1,
+	as: 'h1',
 	children: 'SKF Heading Element Level 1',
 };
 
 export const DefaultH2 = Template.bind({});
 DefaultH2.args = {
-	as: HeadingLevel.h2,
+	as: 'h2',
 	children: 'SKF Heading Element Level 2',
 };
 
 export const DefaultH3 = Template.bind({});
 DefaultH3.args = {
-	as: HeadingLevel.h3,
+	as: 'h3',
 	children: 'SKF Heading Element Level 3',
 };
 
 export const DefaultH4 = Template.bind({});
 DefaultH4.args = {
-	as: HeadingLevel.h4,
+	as: 'h4',
 	children: 'SKF Heading Element Level 4',
 };
 
 export const CustomStyling = () => (
 	<>
-		<Heading as={HeadingLevel.h1} styledAs={HeadingLevel.h2}>
+		<Heading as={'h1'} styledAs={'h2'}>
 			An H1 styled as an H2
 		</Heading>
-		<Heading as={HeadingLevel.h2} styledAs={HeadingLevel.h3}>
+		<Heading as={'h2'} styledAs={'h3'}>
 			An H2 styled as an H3
 		</Heading>
-		<Heading as={HeadingLevel.h3} styledAs={HeadingLevel.h4}>
+		<Heading as={'h3'} styledAs={'h4'}>
 			An H3 styled as an H4
 		</Heading>
-		<Heading as={HeadingLevel.h4} styledAs={HeadingLevel.h1}>
+		<Heading as={'h4'} styledAs={'h1'}>
 			An H4 styled as an H1
 		</Heading>
 	</>
--- src/atoms/heading/Heading.tsx
+++ src/atoms/heading/Heading.tsx
@@ -1,25 +1,13 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import React, { ForwardedRef, forwardRef, HTMLAttributes, ReactNode } from 'react';
-import FontSizes from '~tokens/font-sizes/FontSizes';
-import FontWeights from '~tokens/font-weights/FontWeights';
 import Headings from '~tokens/headings/Headings';
 import LineHeights from '~tokens/line-heights/LineHeights';
 
 /**
  * @category Props
  */
-export enum HeadingLevel {
-	h1 = 'h1',
-	h2 = 'h2',
-	h3 = 'h3',
-	h4 = 'h4',
-}
-
-export type HeadingStyle = {
-	fontSize?: FontSizes;
-	fontWeight?: FontWeights;
-};
+export type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4';
 
 export interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
 	as: HeadingLevel;
@@ -46,31 +34,31 @@ export default Heading;
  */
 type StyledHeadingProps = Pick<HeadingProps, 'as' | 'styledAs'>;
 
-const StyledHeading = styled.h1(({ as, styledAs }: StyledHeadingProps) => {
-	const stylingLevel = styledAs || as;
+const StyledHeading = styled.h1((props: StyledHeadingProps) => {
+	const stylingLevel = props.styledAs || props.as;
 
 	return css`
 		line-height: ${LineHeights.compact};
 
-		${stylingLevel === HeadingLevel.h1 &&
+		${stylingLevel === 'h1' &&
 		css`
 			font-size: ${Headings.h1Size};
 			font-weight: ${Headings.h1Weight};
 		`}
 
-		${stylingLevel === HeadingLevel.h2 &&
+		${stylingLevel === 'h2' &&
 		css`
 			font-size: ${Headings.h2Size};
 			font-weight: ${Headings.h2Weight};
 		`}
 
-		${stylingLevel === HeadingLevel.h3 &&
+		${stylingLevel === 'h3' &&
 		css`
 			font-size: ${Headings.h3Size};
 			font-weight: ${Headings.h3Weight};
 		`}
 
-		${stylingLevel === HeadingLevel.h4 &&
+		${stylingLevel === 'h4' &&
 		css`
 			font-size: ${Headings.h4Size};
 			font-weight: ${Headings.h4Weight};
