3bb95c1b9b037f0a0cb564d4798c75a0f982dda8
Tobias Ljungström <tobias.ljungstrom@gmail.com>
2021-07-19 16:39:10 +0200

    Test(Checkbox): Add test for controlled mode
    Also change existing test to be a little more readable

--- src/atoms/checkbox/Checkbox.test.tsx
+++ src/atoms/checkbox/Checkbox.test.tsx
@@ -1,13 +1,32 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import React from 'react';
+import React, { useState } from 'react';
 import Checkbox from '~atoms/checkbox/Checkbox';
 
 describe('Checkbox', () => {
-	test('The checkbox gets checked when user clicks it', () => {
-		const { getByTitle } = render(<Checkbox id="myCheckbox" title="checkbox" />);
-		const inputElement = getByTitle('checkbox') as HTMLInputElement;
-		userEvent.click(inputElement);
-		expect(inputElement.checked).toBeTruthy();
+	test('The uncontrolled checkbox fires onChange event', () => {
+		const changeHandler = jest.fn();
+		const { getByRole } = render(<Checkbox onChange={changeHandler} />);
+
+		const checkbox = getByRole('checkbox');
+		userEvent.click(checkbox);
+
+		expect(changeHandler).toHaveBeenCalledTimes(1);
+		expect(checkbox).toBeChecked();
+	});
+
+	test('The controlled checkbox fires callback and changes state', () => {
+		const CheckboxComponent = () => {
+			const [checked, setChecked] = useState(false);
+			return <Checkbox checked={checked} onChange={() => setChecked(!checked)} />;
+		};
+
+		const { getByRole } = render(<CheckboxComponent />);
+
+		const checkbox = getByRole('checkbox');
+		expect(checkbox).not.toBeChecked();
+
+		userEvent.click(checkbox);
+		expect(checkbox).toBeChecked();
 	});
 });
