240c14fa2731b182e4aaaefbed4a033f49cb573f
Tobias Ljungström <tobias.ljungstrom@gmail.com>
2021-07-21 14:44:20 +0200

    Feature(Label): Add small and required props

--- src/atoms/label/Label.tsx
+++ src/atoms/label/Label.tsx
@@ -1,64 +1,40 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import React, { ForwardedRef, forwardRef, LabelHTMLAttributes } from 'react';
 import Colors from '~tokens/colors/Colors';
 import FontSizes from '~tokens/font-sizes/FontSizes';
-import FontWeights from '~tokens/font-weights/FontWeights';
-import Spacings from '~tokens/spacings/Spacings';
 
-/**
- * @category Props
- */
 export interface LabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
-	children: React.ReactNode;
-	htmlFor: string;
 	required?: boolean;
+	disabled?: boolean;
+	small?: boolean;
+	children: string;
 }
 
-/**
- * @category Template
- */
 const Label = forwardRef(
 	({ htmlFor, children, required, ...rest }: LabelProps, ref: ForwardedRef<HTMLLabelElement>) => (
 		<StyledLabel {...rest} htmlFor={htmlFor} ref={ref}>
 			{children}
-			{required && <StyledSymbol aria-hidden="true">*</StyledSymbol>}
+			{required && <StyledAsterisk aria-label="Required">*</StyledAsterisk>}
 		</StyledLabel>
 	)
 );
 
-Label.displayName = 'Label';
-export default Label;
+type StyledLabelProps = Pick<LabelProps, 'small' | 'disabled'>;
 
-/**
- * @category Styles
- */
-const StyledLabel = styled.label`
-	color: ${Colors.blackBrand};
-	display: flex;
-	font-weight: ${FontWeights.bold};
-	margin: 0 0 ${Spacings.xxs};
+const StyledLabel = styled.label(
+	({ small, disabled }: StyledLabelProps) => css`
+		color: ${disabled ? Colors.grayDarker : Colors.blackBrand};
+		display: flex;
+		font-size: ${small ? FontSizes.sm : FontSizes.md};
+		line-height: ${small ? 1.3 : 'initial'};
+		margin: 0;
+	`
+);
 
-	&.checkbox-label {
-		cursor: pointer;
-		display: block;
-		font-size: ${FontSizes.lg};
-		font-weight: ${FontWeights.normal} !important;
-		line-height: 1.4;
-		margin-bottom: ${Spacings.sm};
-		padding-left: ${Spacings.xxl};
-		position: relative;
-		-webkit-user-select: none;
-		-moz-user-select: none;
-		-ms-user-select: none;
-		user-select: none;
-	}
-
-	&.small {
-		font-size: ${FontSizes.sm};
-		line-height: 1.3;
-	}
-`;
-
-const StyledSymbol = styled.i`
+const StyledAsterisk = styled.span`
 	color: ${Colors.redBase};
 `;
+
+Label.displayName = 'Label';
+export default Label;
