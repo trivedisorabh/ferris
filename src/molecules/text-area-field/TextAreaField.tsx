90a66056c3347323d3ee359ff04a3c3adcbd628f
Tobias Ljungström <tobias.ljungstrom@gmail.com>
2021-08-17 16:13:41 +0200

    Docs(TSDoc): Add TSDoc comments for interface and component

--- src/molecules/text-area-field/TextAreaField.tsx
+++ src/molecules/text-area-field/TextAreaField.tsx
@@ -5,22 +5,68 @@ import Label, { LabelProps } from '~atoms/label/Label';
 import TextArea, { TextAreaProps } from '~atoms/text-area/TextArea';
 import Colors from '~tokens/colors/Colors';
 
-/**
- * @category Props
- */
 export interface TextAreaFieldProps extends HTMLAttributes<HTMLDivElement> {
-	id: string;
-	label: string;
+	/**
+	 * If provided, this text will appear under the label
+	 */
 	description?: string;
+
+	/**
+	 * If true, the input will be non-interactable
+	 */
 	disabled?: boolean;
+
+	/**
+	 * This id will connect the internal TextArea and its Label
+	 *
+	 * @remarks
+	 * The id must be unique on the page
+	 *
+	 * @see {@link TextArea}
+	 * @see {@link Label}
+	 */
+	id: string;
+
+	/**
+	 * This text will display above the input field
+	 */
+	label: string;
+
+	/**
+	 * These LabelProps will be passed on to the internal Label
+	 *
+	 * @see {@link LabelProps}
+	 * @see {@link Label}
+	 */
 	labelProps?: LabelProps;
+
+	/**
+	 * If true, the input will be set to be required and a visual indicator will appear
+	 */
 	required?: boolean;
+
+	/**
+	 * These TextAreaProps will be passed on to the internal TextArea
+	 *
+	 * @see {@link TextAreaProps}
+	 * @see {@link TextArea}
+	 */
 	textAreaProps?: Partial<TextAreaProps>;
+
+	/**
+	 * Sets the value of the TextArea.
+	 *
+	 * @remarks This prop will make the component controlled and so you must provide an onChange handler
+	 * @see {@link https://reactjs.org/docs/forms.html#controlled-components}
+	 */
 	value?: string;
 }
 
 /**
- * @category Template
+ * TextAreaField component
+ *
+ * @see {@link https://skf.invisionapp.com/dsm/ab-skf/4-web-applications/nav/5fa7caf78c01200018354495/asset/5ea69e3b60bb930a70de53bd | DSM Documentation}
+ * @beta
  */
 const TextAreaField = forwardRef(
 	(
@@ -50,9 +96,6 @@ const TextAreaField = forwardRef(
 TextAreaField.displayName = 'TextAreaField';
 export default TextAreaField;
 
-/**
- * @category Styles
- */
 type StyledTextAreaFieldProps = Pick<TextAreaFieldProps, 'disabled'>;
 
 const StyledTextAreaField = styled.div(
